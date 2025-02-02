80ddfa159c4bf41404ae20575a5a604b81467a7e
Shreeti Biswal <biswalshreeti2005@gmail.com>
2025-02-02 18:01:07 +0530

    equal sum partition added to SubsetSum

--- dynamicProgramming/SubsetSum.java
+++ dynamicProgramming/SubsetSum.java
@@ -35,6 +35,17 @@ public class SubsetSum {
         return Dp[n][sum];
     }
 
+    boolean equalSubsetPartition(int[]arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        
+        if(sum%2==1)return false;
+        
+        return subsetSum(arr,sum/2);
+    }
+
 }
 
 /*Output:-
